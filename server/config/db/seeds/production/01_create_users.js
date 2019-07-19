6c1366d0e40fe05869ef46df2acecd38d3964b3b
Bill Patrianakos <bill@billpatrianakos.me>
2019-07-19 13:23:56 -0500

    Update production seed to use application config so users only have to edit one main config file.

--- server/config/db/seeds/production/01_create_users.js
+++ server/config/db/seeds/production/01_create_users.js
@@ -1,16 +1,18 @@
 // User seeds
 // ==========
+const _      = require('lodash');
+const config = _.merge(require('../../../application').defaults, require('../../../application').production);
 const bcrypt = require('bcrypt');
 
 exports.seed = (knex) => {
   // Delete all existing users before seeding
   return knex('users').del()
-    .then(() => bcrypt.hash('password', 12))
+    .then(() => bcrypt.hash(config.defaultPassword, 12))
     .then((hash) => {
       return knex('users').insert([
         {
           id: 1,
-          username: 'monitor',
+          username: config.defaultUsername,
           password: hash
         }
       ]);
