1463dcff8f9b3808d0aa695455f4d51107a76d4d
Bill Patrianakos <bill@billpatrianakos.me>
2019-07-20 10:39:49 -0500

    Debugging readings query.

--- server/routes/main.js
+++ server/routes/main.js
@@ -6,7 +6,8 @@
 const express        = require('express'),
       HomeController = express.Router(),
       authorize      = require('../lib/authorize'),
-      fs             = require('fs');
+      fs             = require('fs'),
+      Reading        = require(__dirname + '/../models/reading');
 
 HomeController.use(authorize); // Require user to be logged in
 
@@ -14,7 +15,19 @@ HomeController.route('/?')
   // GET /
   // -----
   .get((req, res, next) => {
-    res.render('main');
+    new Reading
+      .orderBy('id', 'desc')
+      .limit(1)
+      .fetch()
+      .then((reading) => {
+console.log('READING -------------------------------=========================');
+console.log(reading);
+        res.render('main', { temperature: reading.get('temperature'), humidity: reading.get('humidity'), created_at: reading.get('created_at') });
+      })
+      .catch((error) => {
+console.log(error);
+        res.status(500).send('Error in knex');
+      });
   });
 
 module.exports = HomeController;
